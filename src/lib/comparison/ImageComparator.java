3393618def4203ac0d4794d9662f7ce11b264241
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-06-02 01:35:36 +0300

    comparator interface

--- src/lib/comparison/ImageComparator.java
+++ src/lib/comparison/ImageComparator.java
@@ -1,11 +1,11 @@
 package lib.comparison;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+
 import org.im4java.core.IM4JavaException;
 import org.im4java.core.IMOperation;
-import org.im4java.core.ImageCommand;
 import org.im4java.core.ImageMagickCmd;
 import org.im4java.process.ArrayListOutputConsumer;
 
@@ -14,11 +14,11 @@ import org.im4java.process.ArrayListOutputConsumer;
  * 
  * @see http://www.imagemagick.org/Usage/compare/
  */
-public class Comparison {
+public class ImageComparator implements Comparator {
 
 	private IMOperation operation;
 	private ArrayListOutputConsumer stdout;
-	private static ImageCommand command = new ImageMagickCmd("compare");
+	private static ImageMagickCmd command = new ImageMagickCmd("compare");
 	private final Metric metric;
 	private String difference;
 
@@ -27,7 +27,7 @@ public class Comparison {
 	 * 
 	 * @param metric the metric to use to compare images
 	 */
-	public Comparison(Metric metric) {
+	public ImageComparator(Metric metric) {
 		this(metric, 0.0D);
 	}
 
@@ -37,7 +37,7 @@ public class Comparison {
 	 * @param metric the metric to use to compare images
 	 * @param fuzz the error acceptance factor
 	 */
-	public Comparison(Metric metric, double fuzz) {
+	public ImageComparator(Metric metric, double fuzz) {
 		this.metric = metric;
 		this.stdout = new ArrayListOutputConsumer();
 		command.setOutputConsumer(this.stdout);
@@ -49,13 +49,7 @@ public class Comparison {
 		this.operation.addImage("null:-");
 	}
 
-	/**
-	 * compare the given images 
-	 * 
-	 * @param imageA image to compare
-	 * @param imageB image to compare with imageA
-	 * @return whether the images are different or not
-	 */
+	@Override
 	public boolean compare(BufferedImage imageA, BufferedImage imageB)
 			throws IOException, InterruptedException, IM4JavaException {
 		command.run(operation, imageA, imageB);
