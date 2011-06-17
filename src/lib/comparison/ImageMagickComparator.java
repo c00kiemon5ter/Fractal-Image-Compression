b6cd64ca7dd8328e511152f5b845b569b6d72493
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-06-17 05:40:28 +0300

    rename ImageComparator to ImageMagickComparator

--- src/lib/comparison/ImageMagickComparator.java
+++ src/lib/comparison/ImageMagickComparator.java
@@ -1,7 +1,6 @@
 package lib.comparison;
 
 import java.io.IOException;
-import java.util.Comparator;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
@@ -15,20 +14,20 @@ import org.im4java.process.ArrayListOutputConsumer;
  * 
  * @see http://www.imagemagick.org/Usage/compare/
  */
-public class ImageComparator implements Comparator<BufferedImage> {
+public class ImageMagickComparator {
 
 	private IMOperation operation;
 	private ArrayListOutputConsumer stdout;
 	private static ImageMagickCmd command = new ImageMagickCmd("compare");
 	private final Metric metric;
-	private String difference;
+	private double difference;
 
 	/**
 	 * Compare two images given a metric
 	 * 
 	 * @param metric the metric to use to compare images
 	 */
-	public ImageComparator(Metric metric) {
+	public ImageMagickComparator(Metric metric) {
 		this(metric, 0.0D);
 	}
 
@@ -38,7 +37,7 @@ public class ImageComparator implements Comparator<BufferedImage> {
 	 * @param metric the metric to use to compare images
 	 * @param fuzz the error acceptance factor
 	 */
-	public ImageComparator(final Metric metric, final double fuzz) {
+	public ImageMagickComparator(final Metric metric, final double fuzz) {
 		this.metric = metric;
 		this.stdout = new ArrayListOutputConsumer();
 		command.setOutputConsumer(this.stdout);
@@ -54,11 +53,23 @@ public class ImageComparator implements Comparator<BufferedImage> {
 	public boolean isEqual(final BufferedImage imageA, final BufferedImage imageB)
 			throws IOException, InterruptedException, IM4JavaException {
 		command.run(operation, imageA, imageB);
-		this.difference = command.getErrorText().get(command.getErrorText().size() - 1).split("\\s+")[0];
-		return this.metric.equalval().equals(this.difference);
+		String result = command.getErrorText().get(command.getErrorText().size() - 1).split("\\s+")[0];
+
+		if (this.metric.equals(Metric.PSNR)) {
+			if (result.equals("inf")) {
+				this.difference = 0;
+			} else {
+				this.difference = Double.POSITIVE_INFINITY - Double.parseDouble(result);
+			}
+		} else if (this.metric.equals(Metric.NCC)) {
+			this.difference = 1 - Double.parseDouble(result);
+		} else {
+			this.difference = Double.parseDouble(result);
+		}
+		return this.difference == 0;
 	}
 
-	public String getDifference() {
+	public double getDifference() {
 		return this.difference;
 	}
 
@@ -76,11 +87,5 @@ public class ImageComparator implements Comparator<BufferedImage> {
 	 */
 	public ArrayList<String> getStderr() {
 		return command.getErrorText();
-	}
-
-	// TODO: compare images
-	@Override
-	public int compare(BufferedImage imageA, BufferedImage imageB) {
-		return 0;
 	}
 }
