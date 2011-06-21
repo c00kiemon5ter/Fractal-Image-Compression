97b9277cb5a42525e1df114fb9dae37c2a7e0dad
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-06-21 18:59:21 +0300

    Compressor: extend Observable to return progress results + a bugfix

--- src/lib/Compressor.java
+++ src/lib/Compressor.java
@@ -18,15 +18,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.Set;
 
 /**
  * fractal compressor instance. combines the tiler and
  * comparator classes to create a fractal image model
- * 
+ *
  * NOTE: the current compressor cannot handle scale transforms
  */
-public class Compressor {
+public class Compressor extends Observable {
 
     private ScaleTransform              scaleTransform;
     private Tiler<BufferedImage>        tiler;
@@ -35,64 +37,61 @@ public class Compressor {
     private Set<BufferedImageOp>        filters;
 
     /**
-     * 
+     *
      * NOTE: the current compressor cannot handle scale transforms
-     * 
+     *
      * @param scaleTransform the scale difference between the ranges and the domains
      * @param tiler the tiler used to tile the image
      * @param comparator the comparator used to compare the tiles of the image
      * @param transforms a list of transform to apply to the tiles of the image
+     * @param observer an observer receiving progress results from {@code compress} - allowed to be null
+     *
      * @throws NullPointerException if any field is null
-     */
-    public Compressor(final ScaleTransform scaleTransform,
-                      final Tiler<BufferedImage> tiler,
-                      final Distanceator<BufferedImage> comparator,
-                      final Set<ImageTransform> transforms) throws NullPointerException {
-        this(scaleTransform, tiler, comparator, transforms, new HashSet<BufferedImageOp>(0));
-    }
-
-    /**
-     * 
-     * NOTE: the current compressor cannot handle scale transforms
-     * 
-     * @param scaleTransform the scale difference between the ranges and the domains
-     * @param tiler the tiler used to tile the image
-     * @param comparator the comparator used to compare the tiles of the image
-     * @param transforms a list of transform to apply to the tiles of the image
-     * @param filter a filter to apply to the image for normalization
-     * @throws NullPointerException if any field is null
+     *
+     * @see #compress(java.awt.image.BufferedImage)
+     * @see Observable
+     * @see Observer#update(java.util.Observable, java.lang.Object)
      */
     public Compressor(final ScaleTransform scaleTransform,
                       final Tiler<BufferedImage> tiler,
                       final Distanceator<BufferedImage> comparator,
                       final Set<ImageTransform> transforms,
-                      final BufferedImageOp filter) throws NullPointerException {
-        this(scaleTransform, tiler, comparator, transforms, new HashSet<BufferedImageOp>(1) {{ add(filter); }});
+                      Observer observer) throws NullPointerException {
+        this(scaleTransform, tiler, comparator, transforms,
+             new HashSet<BufferedImageOp>(0), observer);
     }
 
     /**
-     * 
+     *
      * NOTE: the current compressor cannot handle scale transforms
-     * 
+     *
      * @param scaleTransform the scale difference between the ranges and the domains
      * @param tiler the tiler used to tile the image
      * @param comparator the comparator used to compare the tiles of the image
      * @param transforms a set of transform to apply to the tiles of the image
      * @param filters a set of filters to apply to the image for normalization
+     * @param observer an observer receiving progress results from {@code compress} - allowed to be null
+     *
      * @throws NullPointerException if any field is null
+     *
+     * @see #compress(java.awt.image.BufferedImage)
+     * @see Observable
+     * @see Observer#update(java.util.Observable, java.lang.Object)
      */
     public Compressor(final ScaleTransform scaleTransform,
                       final Tiler<BufferedImage> tiler,
                       final Distanceator<BufferedImage> comparator,
                       final Set<ImageTransform> transforms,
-                      final Set<BufferedImageOp> filters) throws NullPointerException {
+                      final Set<BufferedImageOp> filters,
+                      Observer observer) throws NullPointerException {
         assert (comparator != null) && (transforms != null) && (filters != null)
                && (tiler != null) && (scaleTransform != null) : "Null elements now allowed";
 
         this.comparator = comparator;
         this.tiler      = tiler;
-        this.transforms = transforms;
         this.filters    = filters;
+        this.transforms = transforms;
+        this.scaleTransform = scaleTransform;
 
         /*
          * The 'None' transform -- makes no change to the given image.
@@ -106,6 +105,10 @@ public class Compressor {
                 return inputimage;
             }
         });
+
+        if (observer != null) {
+            this.addObserver(observer);
+        }
     }
 
     /**
@@ -113,9 +116,9 @@ public class Compressor {
      * small images and transforms to points. Applying the transforms
      * to the images and placing the resulted transformed images
      * to the mapped points, the original image is reassembled.
-     * 
+     *
      * NOTE: the current compressor cannot handle scale transforms
-     * 
+     *
      * @param image the image to compress
      * @return a mapping of points to images and transforms.
      */
@@ -134,9 +137,10 @@ public class Compressor {
          * range blocks are the tiles of the original(but filtered) image.
          */
         ArrayList<BufferedImage> rangeblocks = tiler.tile(image);
+        int rangessize = rangeblocks.size();
 
         /*
-         * the domain image to tile, is a scaled factor of the size 
+         * the domain image to tile, is a scaled factor of the size
          * of the original image (scalex * width; scaley * height).
          * domain and range blocks have the same size, but, domain
          * blocks are less in number than range blocks.
@@ -154,18 +158,18 @@ public class Compressor {
          */
         Map<BufferedImage, Map.Entry<BufferedImage, ImageTransform>> domainblocks =
             new HashMap<BufferedImage, Map.Entry<BufferedImage, ImageTransform>>(
-                (int) (transforms.size() * rangeblocks.size() * scaleTransform.getScalex() * scaleTransform.getScaley()));
+                (int) (transforms.size() * rangessize * scaleTransform.getScalex() * scaleTransform.getScaley()));
 
         for (BufferedImage domainImg : tiler.tile(scaleTransform.transform(image))) {
             for (ImageTransform transform : transforms) {
-                domainblocks.put(transform.transform(domainImg), 
+                domainblocks.put(transform.transform(domainImg),
                                  new SimpleEntry<BufferedImage, ImageTransform>(domainImg, transform));
             }
         }
 
         /* A mapping between a range and most suitable domain block */
-        Map<BufferedImage, BufferedImage> rangeDomainMatches = 
-            new HashMap<BufferedImage, BufferedImage>(rangeblocks.size());
+        Map<BufferedImage, BufferedImage> range_domain_matches =
+            new HashMap<BufferedImage, BufferedImage>(rangessize);
 
         /*
          * After the end of each domain loop, or in other words, before
@@ -176,7 +180,8 @@ public class Compressor {
          * (minimum) distance found so far, in the a variable, which is
          * reset in every range image iteration.
          */
-        for (BufferedImage rangeblock : rangeblocks) {
+        for (int rangeidx = 0; rangeidx < rangessize; rangeidx++) {
+            BufferedImage rangeblock = rangeblocks.get(rangeidx);
             double mindiff = Double.MAX_VALUE;
 
             for (BufferedImage domainblock : domainblocks.keySet()) {
@@ -191,11 +196,14 @@ public class Compressor {
                  * than the best (minimum) so far), we update the best
                  * difference and map the range to the new domain image.
                  */
-                if (!rangeDomainMatches.containsKey(rangeblock) || (mindiff > diff)) {
-                    rangeDomainMatches.put(rangeblock, domainblock);
+                if (!range_domain_matches.containsKey(rangeblock) || (mindiff > diff)) {
+                    range_domain_matches.put(rangeblock, domainblock);
                     mindiff = diff;
                 }
             }
+
+            this.setChanged();
+            this.notifyObservers(new int[]{rangeidx, rangessize});
         }
 
         /*
@@ -205,15 +213,15 @@ public class Compressor {
          * the original image.
          * We map each point to a corresponding domain image and transform.
          */
-        Map<Point, Map.Entry<BufferedImage, ImageTransform>> results = 
-            new HashMap<Point, Map.Entry<BufferedImage, ImageTransform>>(rangeblocks.size());
+        Map<Point, Map.Entry<BufferedImage, ImageTransform>> fractalmodel =
+            new HashMap<Point, Map.Entry<BufferedImage, ImageTransform>>(rangessize);
         int width = image.getWidth();
 
-        for (BufferedImage range : rangeDomainMatches.keySet()) {
-            results.put(Utils.indexToPoint(rangeblocks.indexOf(range), width),
-                        domainblocks.get(rangeDomainMatches.get(range)));
+        for (BufferedImage range : range_domain_matches.keySet()) {
+            fractalmodel.put(Utils.indexToPoint(rangeblocks.indexOf(range), width),
+                        domainblocks.get(range_domain_matches.get(range)));
         }
 
-        return results;
+        return fractalmodel;
     }
 }
