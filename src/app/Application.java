201162447db10630f9e13bdb97b06e7a79b76fb0
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-06-26 21:01:13 +0300

    update TODO

--- src/app/Application.java
+++ src/app/Application.java
@@ -6,7 +6,22 @@ import app.configuration.Configuration;
  * Command line utility to compress an image using
  * fractal image compression methods.
  * 
+ * Configuration:
  * TODO: add configuration of filters and transforms
+ * 
+ * Comparators:
+ * FIXME: the current compressor cannot handle scale transforms due to
+ * ImageComparator's limitation to same-size image comparison only
+ * 
+ * Metric:
+ * NOTE: currently implemented: AE MSE RMSE
+ * NOTE: currently only AE is affected by fuzz
+ * 
+ * Compression:
+ * TODO: FUTURE: multi-threading
+ * 
+ * Tilers:
+ * TODO: FUTURE: intelligent tilers - HV/Quadtree partitioning
  */
 public class Application {
 
