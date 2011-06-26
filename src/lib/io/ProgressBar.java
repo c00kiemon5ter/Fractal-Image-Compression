2be436466c9337163e208588db9923e9390ceccc
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-06-26 16:17:06 +0300

    reset progressbar on completion

--- src/lib/io/ProgressBar.java
+++ src/lib/io/ProgressBar.java
@@ -1,7 +1,9 @@
 package lib.io;
 
 /**
- * an ascii progress meter. 
+ * Ascii progress meter. On completion this will reset itself, so it can be reused
+ * <br /><br />
+ * 100% ################################################## |
  */
 public class ProgressBar {
     private StringBuilder progress;
@@ -10,9 +12,9 @@ public class ProgressBar {
      * initialize progress bar properties.
      */
     public ProgressBar() {
-        this.progress = new StringBuilder(60);
+        init();
     }
-    
+
     /**
      * called whenever the progress bar needs to be updated.
      * that is whenever progress was made.
@@ -25,15 +27,22 @@ public class ProgressBar {
         String format = "\r%3d%% %s %c";
 
         int percent = (++done * 100) / total;
-
         int extrachars = (percent / 2) - this.progress.length();
+
         while (extrachars-- > 0) {
             progress.append('#');
         }
 
         System.out.printf(format, percent, progress, workchars[done % workchars.length]);
+
         if (done == total) {
+            System.out.flush();
             System.out.println();
+            init();
         }
+    }
+
+    private void init() {
+        this.progress = new StringBuilder(60);
     }
 }
